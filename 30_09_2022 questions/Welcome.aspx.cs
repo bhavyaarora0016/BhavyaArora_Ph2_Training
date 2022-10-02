f00934dd070d76558f90fb1cc3ac07977207ebcd
Bhavya Arora <45413787+bhavyaarora0016@users.noreply.github.com>
2022-10-02 23:52:02 +0530

    Create Welcome.aspx.cs

---
+++ 30_09_2022 questions/Welcome.aspx.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DAL3009;
+
+namespace webapp1
+{
+    public partial class Welcome : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HttpCookie cookie;
+             cookie = Request.Cookies.Get("CustData");
+            Customer c = new Customer();
+            c.firstName = cookie["fName"];
+            c.lastName = cookie["lName"];
+            c.dob = DateTime.Parse(cookie["dob1"]);
+            c.city = cookie["city1"];
+            c.state = cookie["state1"];
+            lblwelcome.Text = "Welcome " + c.firstName + " " + c.lastName;
+          //ListBox1.Items.Add("Welcome " + c.firstName + " " + c.lastName);
+          //ListBox1.Items.Add(new ListItem { Text = "1", Value = "Welecome Anand" });
+
+        }
+
+      
+    }
+}
