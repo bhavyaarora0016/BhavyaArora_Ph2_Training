d917c73929119e2511b0bdcfd6c25435b79fd9f9
Bhavya Arora <45413787+bhavyaarora0016@users.noreply.github.com>
2022-10-02 23:39:13 +0530

    Create DAL Customer.cs

---
+++ 30_09_2022 questions/DAL Customer.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL3009
+{
+    public class Customer
+    {
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+
+        public DateTime dob { get; set; }
+
+    }
+}
